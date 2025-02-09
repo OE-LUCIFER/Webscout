2aad85fe4431d196aae344e5d9d89d5a620192a0
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-02-09 12:45:44 +0000

    Fix Mentat Scripts: pip3 install -e .[dev]
    Error output:
    =====23 Lines Omitted=====
    
    ==============================
    Full log:[here ❌](https://mentat.ai/log/083b27d2-4a55-4ce0-9ae4-d59d679de81b)

--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -1,3 +1,3 @@
 pip3 install setuptools wheel pip
-pip3 install primp pyreqwest_impersonate httpx curl_cffi nest-asyncio rich markdownify requests google-generativeai lxml termcolor orjson PyYAML tls_client clipman playsound ollama pillow bson cloudscraper html5lib aiofiles emoji openai prompt-toolkit gradio_client psutil yaspin pygetwindow
+pip3 install primp pyreqwest_impersonate httpx curl_cffi nest-asyncio rich markdownify requests google-generativeai lxml termcolor orjson PyYAML tls_client clipman playsound ollama pillow bson cloudscraper html5lib aiofiles emoji openai prompt-toolkit gradio_client psutil yaspin pygetwindow aiohttp pyfiglet
 pip3 install -e .[dev]
